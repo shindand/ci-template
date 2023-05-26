3ac57053aa8804da5b80a8a7167197465affeb3a
warvic <warvic@yandex.ru>
2023-05-26 10:51:12 +0300

    truble

--- ci-template/src/js/__tests__/basic.test.js
+++ ci-template/src/js/__tests__/basic.test.js
@@ -1,7 +1,15 @@
-import sum from '../basic';
+import {sum, mag} from '../basic';
 
 test('should sum', () => {
   const result = sum([1, 2, 3]);
 
   expect(result).toBe(6);
+});
+
+//import mag from '../basic';
+
+test('should mag', () => {
+  const result = healthMag;
+
+  expect(result).toBe('healthy');
 });
--- ci-template/src/js/basic.js
+++ ci-template/src/js/basic.js
@@ -5,3 +5,17 @@ export default function sum(items) {
   }
   return result;
 }
+
+const obj = {name: 'Маг', health: 90};
+
+export default function mag(obj){
+  if (obj.health < 15) {
+    return healthMag = critical;
+  }
+  if (obj.health > 15 && obj.health < 50) {
+    return healthMag = wounded;
+  }
+  if (obj.health > 50) {
+    return healthMag = healthy;
+  }
+}
